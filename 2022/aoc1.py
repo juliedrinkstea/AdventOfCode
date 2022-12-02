af1521481f676e5b3905575f9faa9c2193a06673
Julie 'Pearson' Garst <juliedrinkstea@users.noreply.github.com>
2022-12-02 10:23:10 -0700

    Update aoc1.py

--- 2022/aoc1.py
+++ 2022/aoc1.py
@@ -7,16 +7,11 @@ input = pd.read_csv('~/Documents/Python/aoc2022/aoc1.csv', names=['calories'],sk
 #change NaN in blank rows to 0
 input.fillna(0,inplace=True)
 
-# If calorie value is <1, we know it was one of the blank rows, so add a column we can group by. Increment group by 1 each time calories is <1
+# Add a column to group by. Increment group by 1 each time calories is <1
 input['elf_num'] = (input['calories'] < 1).cumsum()+1
 
 #create a df that groups by assigned elf number. Sum the other column (calories) per group.
 sum_df = input.groupby(['elf_num']).sum()
-
-#Sometimes I'll print things double check what is going on
-#print(input_df.head(10))
-#print(sum_df.head(10))
-#print(sum_df[sum_df['calories']==sum_df['calories'].max()])
 
 
 #part 1 answer
